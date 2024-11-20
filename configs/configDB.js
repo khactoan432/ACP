8f9f9a3237e4a2ee43aa76d8745ce9a373c2fc6e
khactoan432 <80078390+khactoan432@users.noreply.github.com>
2024-11-20 11:10:50 +0700

    1

--- configs/configDB.js
+++ configs/configDB.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGO_URI || "mongodb+srv://ACP:E4vQAb1d8Vo4tbjv@acp.hplft.mongodb.net/?retryWrites=true&w=majority";
+    const mongoURI =
+      process.env.MONGO_URI ||
+      "mongodb+srv://ACP:E4vQAb1d8Vo4tbjv@acp.hplft.mongodb.net/?retryWrites=true&w=majority";
     await mongoose.connect(mongoURI); // Không cần các tùy chọn useNewUrlParser, useUnifiedTopology
     console.log("MongoDB connected successfully!");
   } catch (err) {
