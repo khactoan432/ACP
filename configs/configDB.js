8c8ca5f8e42e77e591ad4190625da49b30794244
Trí Hoàng <tri1234512321@gmail.com>
2024-11-20 11:00:02 +0700

    connect db

--- configs/configDB.js
+++ configs/configDB.js
@@ -3,17 +3,12 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    const mongoURI =
-      process.env.MONGO_URI ||
-      "mongodb+srv://ACP:E4vQAb1d8Vo4tbjv@acp.hplft.mongodb.net/?retryWrites=true&w=majority";
-    await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const mongoURI = process.env.MONGO_URI || "mongodb+srv://ACP:E4vQAb1d8Vo4tbjv@acp.hplft.mongodb.net/?retryWrites=true&w=majority";
+    await mongoose.connect(mongoURI); // Không cần các tùy chọn useNewUrlParser, useUnifiedTopology
     console.log("MongoDB connected successfully!");
   } catch (err) {
     console.error("MongoDB connection error:", err);
-    process.exit(1); // Dừng server nếu không kết nối được
+    process.exit(1);
   }
 };
 
