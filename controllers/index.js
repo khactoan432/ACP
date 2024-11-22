564c7daa0308fb12097ebaeed567af7f2d089ac2
khactoan432 <80078390+khactoan432@users.noreply.github.com>
2024-11-22 11:05:28 +0700

    done

--- controllers/index.js
+++ controllers/index.js
@@ -1,73 +1,178 @@
-const { Comment, Rate, Banner, Achievement, Course, Topic, Lesson, Exam } = require("../models");
+const {
+  Comment,
+  Rate,
+  Banner,
+  Achievement,
+  Course,
+  Topic,
+  Lesson,
+  Exam,
+} = require("../models");
 
-exports.getComments = async (req, res) => {
-  try {
-    const comments = await Comment.find();
-    res.status(200).json(comments);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
+class indexController {
+  // Lấy danh sách comments
+  async getComments(req, res) {
+    try {
+      const { id } = req.params;
+      //id is courseId, id_commented is include idCourse, idExam, idUser other of user'
+      let { limit } = req.query;
+      limit = parseInt(limit);
+      const isValidLimit = limit && limit < 1;
+      let query = Comment.find({ id_commented: id });
 
-exports.getRates = async (req, res) => {
-  try {
-    const rates = await Rate.find();
-    res.status(200).json(rates);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
+      if (isValidLimit) {
+        query = query.limit(limit);
+      }
 
-exports.getBanners = async (req, res) => {
-  try {
-    const banners = await Banner.find();
-    res.status(200).json(rates);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
+      const comments = await query.exec();
 
-exports.getAchievements = async (req, res) => {
-  try {
-    const achievements = await Achievement.find();
-    res.status(200).json(achievements);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+      res.status(200).json(comments);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
   }
-};
 
-exports.getCourses = async (req, res) => {
-  try {
-    const courses = await Course.find();
-    res.status(200).json(courses);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
+  // Lấy danh sách rates
+  async getRates(req, res) {
+    try {
+      const { id } = req.params;
+      //id is courseId, id_commented is include idCourse, idExam, idUser other of user'
+      const { limit } = req.query;
+      limit = parseInt(limit);
+      const { type } = req.query;
 
-exports.getTopics = async (req, res) => {
-  try {
-    const topics = await Topic.find();
-    res.status(200).json(topics);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
+      if (!type || !["COURSE", "EXAM"].includes(type)) {
+        return res
+          .status(400)
+          .json({ error: "Type must be 'COURSE' or 'EXAM'." });
+      }
 
-exports.getLessons = async (req, res) => {
-  try {
-    const lessons = await Lesson.find();
-    res.status(200).json(lessons);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
+      const isValidLimit = limit && limit < 1;
+      let query = Rate.find({ id_rated: id, type });
 
-exports.getExams = async (req, res) => {
-  try {
-    const exams = await Exam.find();
-    res.status(200).json(exams);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+      if (isValidLimit) {
+        query = query.limit(limit);
+      }
+
+      const rates = await query.exec();
+      res.status(200).json(rates);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
   }
-};
+
+  // Lấy danh sách banners
+  async getBanners(req, res) {
+    try {
+      const { limit } = req.query;
+      limit = parseInt(limit);
+      const isValidLimit = limit && limit > 0;
+      const banners = await Banner.find().limit(isValidLimit ? limit : 0);
+
+      res.status(200).json(banners);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+
+  // Lấy danh sách achievements
+  async getAchievements(req, res) {
+    try {
+      const { limit } = req.query;
+      limit = parseInt(limit);
+      const isValidLimit = limit && limit > 0;
+      let achievements = await Achievement.find().limit(
+        isValidLimit ? limit : 0
+      );
+      res.status(200).json(achievements);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+
+  // Lấy danh sách courses
+  async getCourses(req, res) {
+    try {
+      const { id } = req.params;
+      const { limit } = req.query;
+      limit = parseInt(limit);
+
+      if (id) {
+        const course = await Course.findById(id);
+        if (!course) {
+          return res.status(404).json({ error: "Khóa học không tồn tại." });
+        }
+        return res.status(200).json(course);
+      }
+      const isValidLimit = limit && limit > 0;
+      let query = Course.find();
+      if (isValidLimit) {
+        query = query.limit(limit);
+      }
+      const courses = await query.exec();
+      res.status(200).json(courses);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+
+  // Lấy danh sách topics
+  async getTopics(req, res) {
+    try {
+      const { id } = req.params;
+      const { limit } = req.query;
+      limit = parseInt(limit);
+      const isValidLimit = limit && limit > 0;
+      const topics = await Topic.find({ id_course: id }).limit(
+        isValidLimit ? limit : 0
+      );
+      res.status(200).json(topics);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+
+  // Lấy danh sách lessons
+  async getLessons(req, res) {
+    try {
+      const { id } = req.params;
+      const { limit } = req.query;
+      limit = parseInt(limit);
+      const isValidLimit = limit && limit > 0;
+      const lessons = await Lesson.find({ id_topic: id }).limit(
+        isValidLimit ? limit : 0
+      );
+      res.status(200).json(lessons);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+
+  // Lấy danh sách exams
+  async getExams(req, res) {
+    try {
+      const { id } = req.params;
+      const { limit } = req.query;
+      limit = parseInt(limit);
+
+      if (id) {
+        const exams = await Exam.findById(id);
+        if (!exams) {
+          return res.status(404).json({ error: "Đề thi không tồn tại." });
+        }
+        return res.status(200).json(exams);
+      }
+      const isValidLimit = limit && limit > 0;
+      let query = Exam.find();
+      if (isValidLimit) {
+        query = query.limit(limit);
+      }
+      const exams = await query.exec();
+      res.status(200).json(exams);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+}
+
+module.exports = new indexController();
--- routes/index.js
+++ routes/index.js
@@ -14,13 +14,21 @@ router.use("/teachers", teacherRoutes);
 router.use("/admin", adminRoutes);
 router.use("/auth", authRoutes);
 
-router.get("/comments", indexController.getComments);
-router.get("/rates", indexController.getRates);
+//get comments by id cource (limit or all)
+router.get("/comments/:id", indexController.getComments);
+//get rates by id cource
+router.get("/rates/:id", indexController.getRates);
+// get six lastest banner
 router.get("/banner", indexController.getBanners);
+//get six achievements latest
 router.get("/achievements", indexController.getAchievements);
-router.get("/courses", indexController.getCourses);
-router.get("/topics", indexController.getTopics);
+//get cources (limit or all or only)
+router.get("/courses/:id?", indexController.getCourses);
+// get topics by cource
+router.get("/topics/:id", indexController.getTopics);
+// get lessons by topic
 router.get("/lessons", indexController.getLessons);
+//get exems (limit or all)
 router.get("/exams", indexController.getExams);
 
 module.exports = router;
